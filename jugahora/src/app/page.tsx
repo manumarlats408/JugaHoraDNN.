118f9e791db0fd77eb92f32613628922e4f13340
Patricio Guledjian <patogulee@gmail.com>
2024-10-07 23:13:40 -0300

    sacamos seccion de subscirpcion

--- jugahora/src/app/page.tsx
+++ jugahora/src/app/page.tsx
@@ -80,29 +80,6 @@ export default function HomePage() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-green-100">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  ¿Listo para jugar?
-                </h2>
-                <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                  Únete a nuestra comunidad de jugadores de pádel y empieza a disfrutar del juego hoy mismo.
-                </p>
-              </div>
-              <div className="w-full max-w-sm space-y-2">
-                <form className="flex space-x-2">
-                  <Input className="max-w-lg flex-1" placeholder="Ingresa tu email" type="email" />
-                  <Button type="submit">Suscribirse</Button>
-                </form>
-                <p className="text-xs text-gray-500 dark:text-gray-400">
-                  Suscríbete para recibir noticias y ofertas especiales.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">
