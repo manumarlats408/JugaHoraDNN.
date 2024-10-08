b080fcf6f60e4983b685991b6e62c02ec3f37b74
Patricio Guledjian <patogulee@gmail.com>
2024-10-07 22:23:45 -0300

    cambio rutaa logout

--- jugahora/src/app/api/logout/route.ts
+++ jugahora/src/app/api/logout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   // Crear la respuesta y borrar la cookie estableciéndola con maxAge = 0 y expirando la cookie
-  const response = NextResponse.redirect(new URL('/', 'http://localhost:3000')); // Redirige a la página principal
+  const response = NextResponse.redirect(new URL('/')); // Redirige a la página principal
 
   response.cookies.set('token', '', {
     httpOnly: true,
