49ae713a1182a76afcba019fbf295ddc0540d6ff
Patricio Guledjian <patogulee@gmail.com>
2024-10-07 23:15:06 -0300

    sacamos input de subscirpcion

--- jugahora/src/app/page.tsx
+++ jugahora/src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
 import { Calendar, UserPlus, Users } from "lucide-react"
 import Link from "next/link"
 import Image from 'next/image'
