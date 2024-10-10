a5ff4e0c763043eeef67303dc1942f6f070a6f83
manumarlats408 <manumarlats@gmail.com>
2024-10-10 09:31:07 -0300

    cambios prisma.ts

--- jugahora/src/app/api/auth/request-reset/route.ts
+++ jugahora/src/app/api/auth/request-reset/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 import jwt from 'jsonwebtoken';
-import prisma from '../../../../prisma';
+import prisma from '@/lib/prisma';
 
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
--- jugahora/src/app/api/registro/route.ts
+++ jugahora/src/app/api/registro/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '../../../prisma';
+import prisma from '@/lib/prisma';
 import { hash } from 'bcryptjs';
 
 
