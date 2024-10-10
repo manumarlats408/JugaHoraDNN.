b01e0c126b94d142ada3fc7264e7fbc12509d7ec
manumarlats408 <manumarlats@gmail.com>
2024-10-09 21:09:52 -0300

    Update prisma.ts

--- jugahora/src/prisma.ts
+++ jugahora/src/prisma.ts
@@ -1,9 +1,19 @@
 // src/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
+declare global {
+  var prisma: PrismaClient | undefined; // Declaración global para el cliente de Prisma
+}
+
+// Inicializa el cliente de Prisma solo una vez
+const prisma = global.prisma || new PrismaClient({
   log: ['query', 'info', 'warn', 'error'], // Manteniendo los logs
 });
+
+// Asigna a global para evitar nuevas instancias en entornos de desarrollo
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
 
 // Manejo de la desconexión
 const shutdown = async () => {
