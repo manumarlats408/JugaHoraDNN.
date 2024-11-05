ce480a2fae21a4fdb4f3edf6757053efb200a8e1
Patricio Guledjian <patogulee@gmail.com>
2024-11-05 18:37:15 -0300

    console logs

--- jugahora/src/app/api/matches/route.ts
+++ jugahora/src/app/api/matches/route.ts
@@ -5,10 +5,8 @@ export async function POST(request: Request) {
   try {
     const { date, time, court } = await request.json();
 
-    // Verifica si los datos están llegando correctamente
     console.log("Datos recibidos:", { date, time, court });
 
-    // Verificación de datos
     if (!date || !time || !court) {
       console.log("Faltan campos requeridos");
       return NextResponse.json(
@@ -17,14 +15,15 @@ export async function POST(request: Request) {
       );
     }
 
-    // Intenta crear un objeto `Date` explícitamente a partir del campo `date`
-    const formattedDate = new Date(date);
-    console.log("Fecha formateada:", formattedDate);
+    // Intentar conectar y realizar la creación
+    console.log("Intentando conectar a Prisma...");
+    await prisma.$connect();
+    console.log("Conexión establecida");
 
-    // Intenta crear el partido con la fecha formateada
+    console.log("Creando partido...");
     const newMatch = await prisma.partidos_club.create({
       data: {
-        date: formattedDate,
+        date: new Date(date),
         time,
         court,
         players: 0,
@@ -32,9 +31,11 @@ export async function POST(request: Request) {
       },
     });
 
-    console.log("Partido creado con éxito:", newMatch);
-    return NextResponse.json(newMatch);
+    console.log("Partido creado:", newMatch);
+    await prisma.$disconnect();
+    console.log("Desconectado de Prisma");
 
+    return NextResponse.json(newMatch);
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error('Error al crear el partido:', error.message);
