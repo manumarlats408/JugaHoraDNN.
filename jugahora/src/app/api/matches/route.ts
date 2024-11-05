d2be53941799ad8057a698f66d2589edcb0ee4ce
Patricio Guledjian <patogulee@gmail.com>
2024-11-05 16:21:34 -0300

    cambio de nombre en llamada a la tabla

--- jugahora/src/app/api/matches/route.ts
+++ jugahora/src/app/api/matches/route.ts
@@ -5,7 +5,7 @@ import prisma from '@/lib/prisma';
 export async function POST(request: Request) {
   const { date, time, court } = await request.json();
 
-  const newMatch = await prisma.Partidos_club.create({
+  const newMatch = await prisma.partidos_club.create({
     data: {
       date: new Date(date),
       time,
@@ -20,7 +20,7 @@ export async function POST(request: Request) {
 
 // Obtener todos los partidos con espacio disponible (para jugadores)
 export async function GET() {
-  const matches = await prisma.Partidos_club.findMany({
+  const matches = await prisma.partidos_club.findMany({
     where: {
       players: { lt: 4 }, // Partidos con menos de 4 jugadores
     },
