4d9f8809fc95fd30c85d42cf6566584e3452e9b3
manumarlats408 <manumarlats@gmail.com>
2024-11-06 20:44:17 -0300

    Revert "probando hacer funcionar el boton de unirse, ya muestra bien todos los datos del partido"
    This reverts commit f9dc6fa5ffa49ff29e16d2244258e83e3e259998.

--- jugahora/src/app/jugar/page.tsx
+++ jugahora/src/app/jugar/page.tsx
@@ -19,18 +19,6 @@ type Match = {
   nombreClub: string
 }
 
-interface User {
-  id: string;
-  email: string;
-  firstName?: string;
-  lastName?: string;
-  name?: string;
-  phoneNumber?: string;
-  address?: string;
-  age?: number;
-}
-
-
 const elementosMenu = [
   { href: '/menu', etiqueta: 'Menú', icono: Home },
   { href: '/perfil', etiqueta: 'Perfil', icono: User },
@@ -43,7 +31,6 @@ export default function PaginaJuega() {
   const [matches, setMatches] = useState<Match[]>([])
   const referenciaMenu = useRef<HTMLDivElement>(null)
   const router = useRouter()
-  const [userData, setUserData] = useState<User | null>(null);
 
   const alternarMenu = () => setMenuAbierto(!menuAbierto)
 
@@ -81,8 +68,6 @@ export default function PaginaJuega() {
     obtenerPartidos()
   }, [])
 
-  
-
   const manejarCierreSesion = async () => {
     try {
       await fetch('/api/cerrar-sesion', {
@@ -97,20 +82,10 @@ export default function PaginaJuega() {
 
   const manejarUnirsePartido = async (idPartido: number) => {
     try {
-
-       if (!userData) {
-      console.error('Usuario no autenticado');
-      return;
-    }
-
-    const userId = userData.id;
-
       const respuesta = await fetch(`/api/matches/${idPartido}/join`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ userId }), // Asegúrate de enviar el userId aquí
         credentials: 'include',
-      });
+      })
       if (respuesta.ok) {
         setMatches(matches.map(match => 
           match.id === idPartido
