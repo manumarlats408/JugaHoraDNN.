47a731cd648edfd63a0db22eef670565a9512ee4
Patricio Guledjian <patogulee@gmail.com>
2024-11-08 19:15:50 -0300

    ya no hay mas error de autenticacion, pero siguen habiendo errores

--- jugahora/src/app/api/matches/[id]/join/route.ts
+++ jugahora/src/app/api/matches/[id]/join/route.ts
@@ -15,7 +15,7 @@ export async function POST(
     console.log('ID del partido:', matchId);
 
     // Extraemos el valor del token de las cookies
-    const token = cookies().get('token')?.value; // Cambiado a 'token'
+    const token = cookies().get('token')?.value;
     const userId = await verifyAuth(token); // Pasamos solo el token a verifyAuth
 
     if (userId === null) {
@@ -25,15 +25,29 @@ export async function POST(
 
     // Iniciar una transacción para unir al usuario al partido
     const result = await prisma.$transaction(async (prisma) => {
+      console.log('Buscando partido...');
       const match = await prisma.partidos_club.findUnique({
         where: { id: matchId },
         include: { Usuarios: true },
       });
+      console.log('Partido encontrado:', match);
 
-      if (!match) throw new Error('Partido no encontrado');
-      if (match.players >= match.maxPlayers) throw new Error('El partido está completo');
-      if (match.Usuarios.some((user) => user.id === userId)) throw new Error('Ya estás unido a este partido');
+      if (!match) {
+        console.log('Partido no encontrado');
+        throw new Error('Partido no encontrado');
+      }
 
+      if (match.players >= match.maxPlayers) {
+        console.log('El partido está completo');
+        throw new Error('El partido está completo');
+      }
+
+      if (match.Usuarios.some((user) => user.id === userId)) {
+        console.log('Usuario ya está unido al partido');
+        throw new Error('Ya estás unido a este partido');
+      }
+
+      console.log('Actualizando el partido para agregar el usuario');
       // Actualizar el partido para agregar el usuario
       const updatedMatch = await prisma.partidos_club.update({
         where: { id: matchId },
@@ -45,10 +59,12 @@ export async function POST(
         },
         include: { Usuarios: true },
       });
+      console.log('Partido actualizado:', updatedMatch);
 
       return updatedMatch;
     });
 
+    console.log('Transacción completada exitosamente');
     return NextResponse.json(result);
   } catch (error) {
     console.error('Error al unirse al partido:', error);
