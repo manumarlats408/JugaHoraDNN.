2ccdc8ff6e8117f4fbbcaba6460a38580d5d3016
Patricio Guledjian <patogulee@gmail.com>
2024-11-08 19:27:11 -0300

    probamos que funcione el join

--- jugahora/src/app/api/matches/[id]/join/route.ts
+++ jugahora/src/app/api/matches/[id]/join/route.ts
@@ -14,23 +14,22 @@ export async function POST(
     const matchId = parseInt(params.id);
     console.log('ID del partido:', matchId);
 
-    // Extraemos el valor del token de las cookies
+    // Get the token from cookies
     const token = cookies().get('token')?.value;
-    const userId = await verifyAuth(token); // Pasamos solo el token a verifyAuth
+    const userId = await verifyAuth(token);
 
     if (userId === null) {
       console.log('Usuario no autenticado');
       return NextResponse.json({ error: 'No autenticado' }, { status: 401 });
     }
 
-    // Iniciar una transacción para unir al usuario al partido
+    // Use a transaction to ensure atomic operations
     const result = await prisma.$transaction(async (prisma) => {
+      // Fetch the current match details
       console.log('Buscando partido...');
       const match = await prisma.partidos_club.findUnique({
         where: { id: matchId },
-        include: { Usuarios: true },
       });
-      console.log('Partido encontrado:', match);
 
       if (!match) {
         console.log('Partido no encontrado');
@@ -42,25 +41,34 @@ export async function POST(
         throw new Error('El partido está completo');
       }
 
-      if (match.Usuarios.some((user) => user.id === userId)) {
+      // Check if the user is already in the "Usuarios" array
+      if (match.usuarios && match.usuarios.includes(userId)) {
         console.log('Usuario ya está unido al partido');
         throw new Error('Ya estás unido a este partido');
       }
 
-      console.log('Actualizando el partido para agregar el usuario');
-      // Actualizar el partido para agregar el usuario
+      // Update the "Usuarios" array in Partidos_club and increment the player count
       const updatedMatch = await prisma.partidos_club.update({
         where: { id: matchId },
         data: {
           players: match.players + 1,
-          Usuarios: {
-            connect: { id: userId },
+          usuarios: {
+            push: userId, // Add the userId to the Usuarios array
           },
         },
-        include: { Usuarios: true },
       });
-      console.log('Partido actualizado:', updatedMatch);
 
+      // Update the "PartidosUnidos" array in User to include this match
+      await prisma.user.update({
+        where: { id: userId },
+        data: {
+          partidosUnidos: {
+            push: matchId, // Add the matchId to the PartidosUnidos array
+          },
+        },
+      });
+
+      console.log('Partido actualizado:', updatedMatch);
       return updatedMatch;
     });
 
