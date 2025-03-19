afb1aec29b7926b11aa7944ac2bf9ee60227cf18
Patricio Guledjian <patogulee@gmail.com>
2025-03-19 11:36:40 -0300

    primer push para enviar mails

--- jugahora/.next/server/webpack-runtime.js
+++ jugahora/.next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c19ec25217208580")
+/******/ 		__webpack_require__.h = () => ("7f2c2f6c08020a69")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
