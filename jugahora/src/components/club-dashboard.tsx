d5b462a169e5277fb2d695ba8f247d9a372f58e5
Patricio Guledjian <patogulee@gmail.com>
2025-04-04 09:02:40 -0300

    Revert "."
    This reverts commit 1c1befff37182f7359989716eab961834b60d95b.

--- jugahora/src/components/club-dashboard.tsx
+++ jugahora/src/components/club-dashboard.tsx
@@ -390,15 +390,14 @@ export function ClubDashboard() {
               <div className="grid gap-4 py-4">
                 <div className="grid grid-cols-4 items-center gap-4">
                   <Label htmlFor="date" className="text-right">Fecha</Label>
-                  <Popover modal={false}>
+                  <Popover>
                     <PopoverTrigger asChild>
                     <Button
                       variant="outline"
                       className="w-full justify-between font-normal"
                     >
-                      <CalendarIcon className="ml-2 h-4 w-4" />
                       {editSelectedDate ? format(editSelectedDate, "dd/MM/yyyy") : "Seleccionar fecha"}
-                      
+                      <CalendarIcon className="ml-2 h-4 w-4" />
                     </Button>
 
                     </PopoverTrigger>
@@ -421,7 +420,6 @@ export function ClubDashboard() {
                       }}
                       showOutsideDays={false}
                       initialFocus
-
                     />
                     </PopoverContent>
                   </Popover>
