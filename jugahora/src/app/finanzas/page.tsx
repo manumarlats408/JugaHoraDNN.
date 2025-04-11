b0560e2be9a93a2377f58719554659231b7f19c6
manumarlats408 <manumarlats@gmail.com>
2025-04-10 23:14:04 -0300

    me falto el sidebar

--- jugahora/src/app/finanzas/page.tsx
+++ jugahora/src/app/finanzas/page.tsx
@@ -1,5 +1,13 @@
-import MovimientosFinancieros from "@/components/movimientos-financieros";
+import MovimientosFinancieros from "@/components/movimientos-financieros"
+import { Sidebar } from "@/components/layout/sidebar"
 
-export default function FinanzasPage() {
-  return <MovimientosFinancieros />;
+export default function MovimientosPage() {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <main className="flex-1 overflow-auto p-8">
+        <MovimientosFinancieros />
+      </main>
+    </div>
+  )
 }
